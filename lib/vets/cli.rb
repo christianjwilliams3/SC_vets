3ad59086b19c034b2cf7d25f04623483756fbd48
christian williams <christian.williamsj@gmail.com>
2019-11-05 04:33:29 +0000

    update

--- lib/vets/cli.rb
+++ lib/vets/cli.rb
@@ -3,16 +3,14 @@ class Vets::CLI
   attr_accessor :all_vets
 
   def intro
+    fetch_vets
     puts "Welcome, Which hospitals interest you?"
     user_choice = ""
+    list_vets #listed vets
     while user_choice != "exit" #verify not exit
-      list_vets #listed vets
-      puts "Which hospital interests you?"
       user_choice = user_input #asked which one they want to see
       show_vet(user_choice.to_i) #show what they chose
-      puts "Enter another vet number or exit to finish, push exit to quit"
-      user_choice = user_input 
-    end
+      puts "Enter another a vet index or exit to finish, push exit to quit"    
     goodbye
   end
 
@@ -22,13 +20,15 @@ class Vets::CLI
   end
 
   def list_vets
-    vets=Vets::API.new #Creates a new API object
-    vets.get_vets # gets all the vets in the array with return
     @all_vets = Vets::Vets.all
     @all_vets.each_with_index do |vet,index| #Allows a loop over the array
       puts "#{index + 1}. #{vet.name}" # Index of Vets name
     end
+  end
 
+  def fetch_vets
+    vets=Vets::API.new #Creates a new API object
+    vets.get_vets # gets all the vets in the array with return
   end
 
   def user_input
